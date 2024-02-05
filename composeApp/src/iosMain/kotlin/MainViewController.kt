ea506d2da187f5cead1e6907eaea9fc16bbddafc
qasimnawaz <qasimnawaz019@gmail.com>
2024-02-05 19:37:39 +0500

    MainViewController DefaultComponentContext updated for ios lifecycle events

--- composeApp/src/iosMain/kotlin/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/MainViewController.kt
@@ -1,13 +1,16 @@
 import androidx.compose.runtime.remember
 import androidx.compose.ui.window.ComposeUIViewController
 import com.arkivanov.decompose.DefaultComponentContext
+import com.arkivanov.decompose.ExperimentalDecomposeApi
+import com.arkivanov.decompose.lifecycle.ApplicationLifecycle
 import com.arkivanov.essenty.lifecycle.LifecycleRegistry
 import presentation.screens.main.root.App
 import presentation.screens.main.root.RootComponent
 
+@OptIn(ExperimentalDecomposeApi::class)
 fun MainViewController() = ComposeUIViewController {
     val root = remember {
-        RootComponent(DefaultComponentContext(LifecycleRegistry()))
+        RootComponent(DefaultComponentContext(ApplicationLifecycle()))
     }
     App(root)
 }
